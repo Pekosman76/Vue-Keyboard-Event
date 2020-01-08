bad04588b3291f1dc168abf44a29909891f81218
Alexandre <a.oursel.14@groupe-esigelec.fr>
2020-01-08 09:56:25 +0100

    Add vue keyboard event component

---
+++ vue-keyboard-event.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    getKeyboard: function(event) {
+      console.log(event.key)
+    }
+  },
+  mounted () {
+    window.addEventListener("keydown", this.getKeyboard);
+  }
+}
+</script>
+
+<style>
+
+</style>
